e4b0acf072811960bcdab7d96e79c2d3ad2e37be
kokiSAT0 <kokisato0123@gmail.com>
2025-07-17 05:14:00 +0900

    docs: add unused method comments

--- learning_all_in_one.py
+++ learning_all_in_one.py
@@ -429,18 +429,23 @@ class RandomAgent:
         return random.choice(valid_actions)
 
     def record_transition(self, s, a, r, s_next, done):
+        """このエージェントでは未使用"""
         pass
 
     def record_reward(self, r):
+        """このエージェントでは未使用"""
         pass
 
     def finish_episode(self):
+        """このエージェントでは未使用"""
         pass
 
     def save_model(self, path=MODEL_DIR / "random_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
     def load_model(self, path=MODEL_DIR / "random_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
 
@@ -513,18 +518,23 @@ class ImmediateWinBlockAgent:
         return False
 
     def record_transition(self, s, a, r, s_next, done):
+        """このエージェントでは未使用"""
         pass
 
     def record_reward(self, r):
+        """このエージェントでは未使用"""
         pass
 
     def finish_episode(self):
+        """このエージェントでは未使用"""
         pass
 
     def save_model(self, path=MODEL_DIR / "immediate_win_block_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
     def load_model(self, path=MODEL_DIR / "immediate_win_block_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
 
@@ -602,18 +612,23 @@ class FourThreePriorityAgent:
         return False
 
     def record_transition(self, s, a, r, s_next, done):
+        """このエージェントでは未使用"""
         pass
 
     def record_reward(self, r):
+        """このエージェントでは未使用"""
         pass
 
     def finish_episode(self):
+        """このエージェントでは未使用"""
         pass
 
     def save_model(self, path=MODEL_DIR / "four_three_priority_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
     def load_model(self, path=MODEL_DIR / "four_three_priority_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
 
@@ -670,18 +685,23 @@ class LongestChainAgent:
         return max_len
 
     def record_transition(self, s, a, r, s_next, done):
+        """このエージェントでは未使用"""
         pass
 
     def record_reward(self, r):
+        """このエージェントでは未使用"""
         pass
 
     def finish_episode(self):
+        """このエージェントでは未使用"""
         pass
 
     def save_model(self, path=MODEL_DIR / "longest_chain_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
     def load_model(self, path=MODEL_DIR / "longest_chain_agent.pth"):
+        """このエージェントでは未使用"""
         pass
 
 
@@ -1003,6 +1023,7 @@ class QAgent:
         pass
 
     def finish_episode(self):
+        """このエージェントでは未使用"""
         pass
 
     def save_model(self, path=MODEL_DIR / "q_agent.pth"):
